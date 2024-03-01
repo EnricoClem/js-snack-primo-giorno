4a9a101113accfda463a524c08cbd9acc87a6454
EnricoClem <enrico.clem94@gmail.com>
2024-03-01 16:56:09 +0100

    ADD - exercise 2

--- app.js
+++ app.js
@@ -3,3 +3,11 @@
 var number = 100 //number
 
 console.log (number)
+
+
+
+// Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+const P1 = 3.14
+
+console.log (P1)
